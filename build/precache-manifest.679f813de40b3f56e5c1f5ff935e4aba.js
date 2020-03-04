6b858ad22c36ab5f31fc8d5cc5aa723f9d227777
EylonKoenig <eylonkoenigm@gmail.com>
2020-03-04 13:52:59 +0200

    server

---
+++ build/precache-manifest.679f813de40b3f56e5c1f5ff935e4aba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12220b4cf3a5845334e73f3dd608b783",
+    "url": "/index.html"
+  },
+  {
+    "revision": "838def5051a1f29e7621",
+    "url": "/static/css/main.6449b885.chunk.css"
+  },
+  {
+    "revision": "6e5a9a60ca432bde3e83",
+    "url": "/static/js/2.b29306a7.chunk.js"
+  },
+  {
+    "revision": "e1345d47b220a41451b8c23eb83bc290",
+    "url": "/static/js/2.b29306a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "838def5051a1f29e7621",
+    "url": "/static/js/main.33e4f0fa.chunk.js"
+  },
+  {
+    "revision": "cda345fb50b7e11c0025",
+    "url": "/static/js/runtime-main.844668cf.js"
+  }
+]);
